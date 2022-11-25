76ff9ba4b4f003c836e92120952eec7fa20ba873
Severn Everett <severn.everett@gmail.com>
2022-11-25 09:21:54 +0100

    Updated JDK to 17; Upgraded Kotlin to 1.7.21; Upgraded Spring Boot to 3.0.0; Upgraded JUnit to 5.9.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.6.0"
-    kotlin("plugin.serialization") version "1.6.0"
-    id("org.springframework.boot") version "2.6.0"
+    kotlin("jvm") version "1.7.21"
+    kotlin("plugin.serialization") version "1.7.21"
+    id("org.springframework.boot") version "3.0.0"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
@@ -20,9 +20,9 @@ apply(plugin = "io.spring.dependency-management")
 tasks {
     withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java).configureEach {
         kotlinOptions {
-            jvmTarget = "16"
-            apiVersion = "1.6"
-            languageVersion = "1.6"
+            jvmTarget = "17"
+            apiVersion = "1.7"
+            languageVersion = "1.7"
         }
     }
     test {
@@ -34,14 +34,15 @@ dependencies {
     val junitVersion: String by project
     val kotlinCoroutinesVersion: String by project
     // Production dependencies
-    implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:$kotlinCoroutinesVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation(kotlin("reflect"))
     implementation(kotlin("stdlib-jdk8"))
     // Testing dependencies
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
